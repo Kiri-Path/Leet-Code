1aa0bad2a6abeda8cbc4ad3bae341532d44c22fb
Kiri-Path <38130515+Kiri-Path@users.noreply.github.com>
2024-06-08 14:06:09 +0100

    Update Remove Duplicates from Sorted Array.py

--- Remove Duplicates from Sorted Array.py
+++ Remove Duplicates from Sorted Array.py
@@ -1,3 +1,6 @@
+### SOLUTION 2 :  O(n)  time , O(N) space since we re-create list
+
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
 
@@ -8,3 +11,22 @@ class Solution:
 
         nums[:] = sorted(set(nums))
         return len(nums)
+
+
+
+### SOLUTION 2 :  O(n)  time , O(1) space since we are doing the change  IN PLACE
+
+here we create left and right pointer.
+right pointer is going to go along list and check if the one before is the same
+Left pointer is going to hold the position of the string which needs to be swapped out after the right pointer identifies the duplicate.
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+
+        l = 1
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r-1]:
+                nums[l] = nums[r]
+                l += 1
+        return l
