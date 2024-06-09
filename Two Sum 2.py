fc3d42db68160f2d6919160c0bf1b5d5bafd65cb
Kiri-Path <38130515+Kiri-Path@users.noreply.github.com>
2024-06-09 10:05:16 +0100

    Update Two Sum 2.py

--- Two Sum 2.py
+++ Two Sum 2.py
@@ -3,10 +3,22 @@
 # Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
 
 
+
+### SOLUTION
+
+### APPROACH
+
+#the key difference between this question and 2 sum, is that our array is sorted!
+# so we can a 2 pointer algorithm, i.e we can have a right and left pointer starting from either end of the array
+# if the sum of the 2 end points is less than or greater than the target then we just need to move the left pointer up or right pointer down respectively. 
+# this is time complexity O(n) and space compelxity of O(1) constant since we are not storing anything 
+
+
+
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-#the key difference between this question and 2 sum, is that our array is sorted!
+
 
         #2 pointer algorithm
         l = 0
