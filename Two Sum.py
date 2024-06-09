a06f3e1c96596b27b43c824bbf3f2c702aa01704
Kiri-Path <38130515+Kiri-Path@users.noreply.github.com>
2024-06-09 10:40:32 +0100

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -40,13 +40,14 @@ class Solution:
 
         #create hashmap
         h ={}
-
-        for i in range(len(nums)): # go through all valid indicies
+        n = len(nums)
+        
+        for i in range(n): # go through all valid indicies
             h[nums[i]] = i #set the nums value at index i  as the key and then give it a value of the index 
             # note if we see duplicate numbers it will update the value with latest index.
         
 
-        for i in range(len(nums)):
+        for i in range(n):
             y = target -nums[i] # we want to check if y exists in h and we want to ensure we are not using the same index! 
 
             if y in h and h[y] != i:
