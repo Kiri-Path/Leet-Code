eb90b3162de5a44695a33c356e4ccae7bb784194
Kiri-Path <38130515+Kiri-Path@users.noreply.github.com>
2024-06-10 08:24:08 +0100

    Update Least Number of Unique Integers after K Removals.py

--- Least Number of Unique Integers after K Removals.py
+++ Least Number of Unique Integers after K Removals.py
@@ -1,5 +1,13 @@
 #LEET CODE: Least Number of Unique Integers after K Removals
 
+
+# approach: 
+# create a dictionary of numbers and the frequency they appear in
+# Then create a unique count field which will the length of the list of all the freqnecy values
+# now the important bit... if the frequency value is less than the value of k then we need to reduce the the count
+# because we would remove that many integers, then we need to reduce k by that many values.
+
+
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
 
